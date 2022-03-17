a7d31a2187bcd470f53e310dc042ce30900f689f
Ella Kharakh <ella.kharakh@live.biu.ac.il>
2022-03-17 23:23:53 +0200

    new changes in Game classes

---
+++ src/main/java/com/game/taki/GameController.java
@@ -0,0 +1,32 @@
+package com.game.taki;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class GameController implements IController {
+    private Model model;
+    private Application view;
+    private Stage stage;
+
+    @FXML
+    private void initialize() {
+    }
+
+    public void setModel(Model m) {
+        this.model = m;
+    }
+
+    public void setView(Application v) {
+        this.view = v;
+    }
+
+    public void setStage(Stage s) {
+        this.stage = s;
+    }
+}
--- src/main/java/com/game/taki/GameModel.java
+++ src/main/java/com/game/taki/GameModel.java
@@ -3,5 +3,25 @@ package com.game.taki;
 public class GameModel {
     private Deck deck;
     private Player[] players;
+    public GameModel(Deck deck, Player[] players){
+        this.deck = deck;
+        this.players = players;
+    }
 
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
 }
