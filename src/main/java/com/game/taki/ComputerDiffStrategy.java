a5503e563e28b9d0063585d4bf2ccab3557ed19f
Ella Kharakh <ella.kharakh@live.biu.ac.il>
2022-03-20 19:30:01 +0200

    fixing bugs and working on Strategy Pattern

--- src/main/java/com/game/taki/ComputerDiffStrategy.java
+++ src/main/java/com/game/taki/ComputerDiffStrategy.java
@@ -6,12 +6,18 @@ public class ComputerDiffStrategy implements AIStrategy {
 
     @Override
     public void doOperation(Player p, ICard current, GameModel game) {
+        boolean ok = false;
         CardsCollection playersCards = p.getCardsCollect();
         for(ICard card : playersCards){
             if (card.isValidAction(current)){
                 card.doAction(game);
+                ok = true;
                 break;
             }
+        }
+
+        if(!ok){
+            game.takingCardFromDeck(p);
         }
     }
 }
--- src/main/java/com/game/taki/RealPersonStrategy.java
+++ src/main/java/com/game/taki/RealPersonStrategy.java
@@ -4,6 +4,12 @@ public class RealPersonStrategy implements AIStrategy{
     @Override
     public void doOperation(Player p, ICard current, GameModel game) {
         //ICard card = game.getChosenCard();
-        //card.doAction(game);
+        //if(card.isValidAction(current)){
+        //    card.doAction(game);
+        //}
+        // else{
+        //     game.takingCardFromDeck(p);
+        // }
+
     }
 }
